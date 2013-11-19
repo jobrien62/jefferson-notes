d79c5b761edaf94e36eaf148d2a680fd8b2901ea
Wayne Graham <wayne.graham@virginia.edu>
2013-11-19 14:22:00 -0500

    Update landing page
    Updates landing page to pull images from Fedora

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
     @witness = Witness.first
+
+    @images = Image.where(witness_id: @witness.id).limit(20).offset(6)
   end
 
   def help
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def fedora_url(pid, width=400, height=600)
+    "http://fedoraproxy.lib.virginia.edu/fedora/objects/#{pid}/methods/djatoka:StaticSDef/getScaled?maxWidth=#{width}&maxHeight=#{height}"
+  end
 end
