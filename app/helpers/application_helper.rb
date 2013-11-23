bd29a780e4e3c1ba6c82ece88bb2aded967417b7
Wayne Graham <wayne.graham@virginia.edu>
2013-11-23 10:12:43 -0500

    Adds a helper method for fedora thumbnails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     "http://fedoraproxy.lib.virginia.edu/fedora/objects/#{pid}/methods/djatoka:StaticSDef/getScaled?maxWidth=#{width}&maxHeight=#{height}"
   end
 
+  def fedora_thumb(pid)
+    "http://fedoraproxy.lib.virginia.edu/fedora/objects/#{pid}/methods/djatoka:StaticSDef/getThumbnail"
+  end
+
 end
