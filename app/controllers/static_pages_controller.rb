eaf9d63a908ad67a5e54fcc3b5bcf1fe60dc4822
Wayne Graham <wayne.graham@virginia.edu>
2013-12-10 16:34:21 -0500

    Clean up controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
     @witness = Witness.first
-    @pages = @witness.pages
-    @images = Image.where(witness_id: @witness.id).limit(20).offset(6)
+    #@pages = @witness.pages
+    #@images = Image.where(witness_id: @witness.id).limit(20).offset(6)
   end
 
   def help
