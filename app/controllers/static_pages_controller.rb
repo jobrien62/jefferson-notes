61bae524c085e49135ea74bca918a56f5f465106
Wayne Graham <wayne.graham@virginia.edu>
2013-12-12 14:37:51 -0500

    Update landing page read link
    Point the 'read notes' link to go to the milestone TOC

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @page = Page.first
+    @milestones = Milestone.all
     #@witness = Witness.first
     #@pages = @witness.pages
     #@images = Image.where(witness_id: @witness.id).limit(20).offset(6)
