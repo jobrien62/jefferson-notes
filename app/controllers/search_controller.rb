d46bb0489ec546c9c96ec4808c2c8df9bd26189e
Wayne Graham <wayne.graham@virginia.edu>
2014-01-29 14:29:49 -0500

    Implements search interface

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,14 @@
+require 'rsolr'
+
 class SearchController < ApplicationController
   def index
+
   end
 
   def results
+    solr = RSolr.connect :url => ENV['SOLR_URL']
+    @response = solr.get 'select', :params => {
+      :q=>params[:q]
+    }
   end
 end
