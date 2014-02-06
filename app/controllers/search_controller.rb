b8c76338d12766d5ccdda90d1e00356edac38033
Wayne Graham <wayne.graham@virginia.edu>
2014-02-06 09:44:25 -0500

    protect url search parameters

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,9 +8,22 @@ class SearchController < ApplicationController
   def results
     solr = RSolr.connect :url => ENV['SOLR_URL']
 
+    #params[:q] = "*:*" if params[:q] == ''
+    #params[:q] ||= "*:*"
+    guard_search_params(params)
+
     @response = solr.paginate params[:page], 20, 'select', :params => {
       :q => params[:q],
       :hl => true,
     }
+  end
+
+  private
+
+  def guard_search_params(params)
+    params[:q] = "*:*" if params[:q] == ''
+    params[:q] ||= "*:*"
+
+    params
   end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @page = Page.first
     @milestones = Milestone.all
-    #@witness = Witness.first
-    #@pages = @witness.pages
-    #@images = Image.where(witness_id: @witness.id).limit(20).offset(6)
   end
 
   def help
