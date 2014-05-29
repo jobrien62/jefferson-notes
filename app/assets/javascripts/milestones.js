9909a88a0385d1a89a8512725c8b1c414e28b122
Wayne Graham <wayne.graham@virginia.edu>
2014-05-29 14:43:13 -0400

    Add Scroll to Top
    Implements an unobtrusive scroll to the top of the page
    
    closes #70

--- app/assets/javascripts/milestones.js
+++ app/assets/javascripts/milestones.js
@@ -1,6 +1,22 @@
 "use strict"
 $(document).ready(function() {
 
+  var offset = 200;
+  var duration = 500;
+
+  $(window).scroll(function() {
+    if ( $(this).scrollTop() > offset) {
+      $('.scroll-to-top').fadeIn(duration);
+    } else {
+      $('.scroll-to-top').fadeOut(duration);
+    }
+  });
+
+  $('.scroll-to-top').click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({scrollTop: 0}, duration);
+    //return false;
+  });
 
   $('.authorNote').hide();
 
