2b9c28ce205bedffc534b6d0a17b6e1a52b4dc12
Thomas Nys <nysthee@gmail.com>
2015-04-08 21:50:14 +0200

    Replaces deprecated before_filter with before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :set_milestones
+  before_action :set_milestones
 
   def set_milestones
     @milestones ||= Milestone.all
